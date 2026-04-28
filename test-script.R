6aefb287afa1827be41f5f649c616d65a34668e5
“emma-ras” <emmarasmussen@ucsb.edu>
2026-04-28 08:31:57 -0700

    upload new file

--- test-script.R
+++ test-script.R
@@ -1,1 +1,9 @@
 #Testing script please work
+# Testing new packages
+install.packages("praise")
+install.packages("cowsay")
+install.packages("beepr")
+library(praise)
+library(cowsay)
+library(beepr)
+say("All done making some beautiful tables! :) ", "whale"); beep(3) 
